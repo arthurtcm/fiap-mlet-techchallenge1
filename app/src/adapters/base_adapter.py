0dadbaa2bbf1e80f6fe62660005aa7589c88a2a8
arthurtcm <arthurtcm@gmail.com>
2025-11-03 17:11:07 -0300

    Atualizando app

--- app/src/adapters/base_adapter.py
+++ app/src/adapters/base_adapter.py
@@ -3,12 +3,12 @@ import pandas as pd
 
 class BaseAdapter:
     def ler_base_categorias(self):
-        df = pd.read_csv('../bases/categorias.csv', sep=',')
+        df = pd.read_csv('bases/categorias.csv', sep=',')
 
         return df
 
     def ler_base_livros(self):
-        df = pd.read_csv('../bases/base_livros.csv', sep=',')
+        df = pd.read_csv('bases/base_livros.csv', sep=',')
         df['id'] = df.index
 
         return df
